1a8ef4d8ee5fe95aba3c482ded0a26e0ecd1f58f
Brandon Jimenez <jiminikiz@gmail.com>
2016-08-10 17:55:49 -0600

    QuickCommit

--- routes/index.js
+++ routes/index.js
@@ -10,17 +10,16 @@ module.exports = function(app) {
         } else {
             res.redirect('/login')
         }
-    })
+    });
+    app.get('/login', Auth.render); // route for the login page
+    app.get('/logout', Auth.logout); // route for logging out
 
-    app.get('/login', Auth.render) // route for the login page
-    app.get('/logout', Auth.logout) // route for logging out
-
-    app.post('/login', Auth.login) // form request emdpoint for loggin in
-    app.post('/register', Auth.register) // form request endpoint for user registration
+    app.post('/login', Auth.login); // form request emdpoint for loggin in
+    app.post('/register', Auth.register); // form request endpoint for user registration
 
     // DAHSBOARD
-    app.all('/dashboard*', Auth.session) // protect all dashboard routes from unauthorized users
+    app.all('/dashboard*', Auth.session); // protect all dashboard routes from unauthorized users
     app.get('/dashboard', (req, res) => { // renders the dashboard
         res.render('dashboard.html', req.session)
-    })
+    });
 }
--- server.js
+++ server.js
@@ -12,7 +12,6 @@ var express = require('express'), // our framework!
         requestKey: 'session', // stores the session cookie in req.session
         duration: 86400, // one week in seconds = 60 * 60 * 24
         cookie: {
-            path:'/',           // set cookie on the root of the site so that all routes possess the cookie in `req.session`
             ephemeral: false,   // when true, cookie expires when the browser closes
             httpOnly: true,     // when true, cookie is not accessible from javascript
             secure: false       // when true, cookie will only be sent over SSL;
